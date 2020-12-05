b3c61e96f912cebb421e2dd4fa05a30b51bdd915
Nima Mahmoudi <nima_mahmoudi@live.com>
2020-12-05 15:01:32 +0000

    added extra settings to dev setup

--- experiments/logger/dev.sh
+++ experiments/logger/dev.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # load environment variables
-# export $(cat .env.dev | grep ^[A-Z] | xargs)
+export $(cat .env.dev | grep ^[A-Z] | xargs)
+
+# Home directory
+export HOME_DIR=${HOME_DIR:-$(pwd)}
 
 docker-compose -f docker-compose-dev.yml up --build
 
