482cb113033651cd166d07310db051a47c92e3be
Nima Mahmoudi <nima_mahmoudi@live.com>
2020-12-06 01:40:24 +0000

    cleaning up bench1 workload

--- experiments/workloads/bench1/src/helpers/logger.py
+++ experiments/workloads/bench1/src/helpers/logger.py
@@ -64,14 +64,6 @@ async def connect():
     await conn_client_info()
 
 
-@sio.event
-async def pong_from_server():
-    global start_timer
-    latency = time.time() - start_timer
-    print('latency is {0:.2f} ms'.format(latency * 1000))
-    await sio.sleep(1)
-
-
 async def start_server():
     await sio.connect(sio_server)
     while True:
