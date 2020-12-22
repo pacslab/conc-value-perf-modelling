2a2888476f1367e96f99bb4c90716fdc3a53016f
Nima Mahmoudi <nima_mahmoudi@live.com>
2020-12-22 22:37:20 +0000

    improved concurrency testing

--- experiments/requester/conc_test.sh
+++ experiments/requester/conc_test.sh
@@ -16,5 +16,11 @@ export HOME_DIR=${HOME_DIR:-$(pwd)}
 docker-compose -f docker-compose-dev.yml up --build -d
 # run test
 docker-compose -f docker-compose-dev.yml exec --user root worker python conc_tester.py $1
+# tear everything down
+docker-compose -f docker-compose-dev.yml down -v
 
-curl --upload-file "src/conc_results/res_$1.csv" https://free.keep.sh
+# curl --upload-file "src/conc_results/res_$1.csv" https://free.keep.sh
+rm conc_tmp.zip
+zip conc_tmp.zip src/conc_results/*.csv
+# automatic upload makes it easier to transfer files
+curl --upload-file ./conc_tmp.zip https://free.keep.sh
