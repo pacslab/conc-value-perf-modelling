c48edd3e9b4185287e00b3ec4a45f7adf5037af0
Nima Mahmoudi <nima_mahmoudi@live.com>
2021-01-03 14:42:56 -0500

    moved functions to separate files

--- model/concperf/single_container_model.py
+++ model/concperf/single_container_model.py
@@ -1,4 +1,5 @@
 import itertools
+import numpy as np
 
 # generate states and encode/decode between idx and state
 class StateCoder:
@@ -33,3 +34,36 @@ class StateCoder:
   def get_state_name_list(self):
     return [f"{s[0]},{s[1]},{s[2]}" for s in self.state_list]
 
+
+# calculate Q for states
+def get_single_container_q(single_coder, config):
+    state_count = single_coder.get_state_count()
+    Q = np.zeros((state_count, state_count))
+
+    def encode_state(req_count):
+        return (req_count,)
+
+    # for each source state calculate rate of destination state
+    for from_state in single_coder.get_state_list():
+        from_state_idx = single_coder.to_idx(from_state)
+        # decode the state
+        (from_req_count, ) = from_state
+        # rate of exiting this state
+        exit_rate = 0
+        # one count below
+        if from_req_count > 0:
+            to_req_count = from_req_count - 1
+            to_state = encode_state(to_req_count)
+            to_state_idx = single_coder.to_idx(to_state)
+            Q[from_state_idx, to_state_idx] = from_req_count / (1+(from_req_count - 1)*config['alpha']) / config['base_service_time']
+            exit_rate += Q[from_state_idx, to_state_idx]
+        if from_req_count < config['max_conc']:
+            to_req_count = from_req_count + 1
+            to_state = encode_state(to_req_count)
+            to_state_idx = single_coder.to_idx(to_state)
+            Q[from_state_idx, to_state_idx] = config['arrival_rate_server']
+            exit_rate += Q[from_state_idx, to_state_idx]
+        
+        Q[from_state_idx, from_state_idx] = -1 * exit_rate
+
+    return Q
--- model/concperf/utility.py
+++ model/concperf/utility.py
@@ -16,3 +16,39 @@ def plot_normal_fit(vals, probs, avg_count, label='Normal Distribution Fit'):
   plt_x_vals = np.linspace(normal_mean - 3 * normal_std, normal_mean + 3 * normal_std, 100)
   dist_vals = np.array([stats.norm.pdf(x, loc=normal_mean, scale=normal_std) for x in plt_x_vals]) # calculate the value of distribution in different configs
   plt.plot(plt_x_vals, dist_vals, label=label)
+
+# calculate probability for adding valuees with similar distribution
+def get_averaged_distribution(vals, probs, avg_count=2, prune_val=1e-6):
+    dist = {vals[i]: probs[i] for i in range(len(vals))}
+
+    for _ in range(avg_count-1):
+        # single addition
+        new_dist = {}
+        for (val, prob) in zip(vals, probs):
+            for k in dist:
+                prob_val = prob * dist[k]
+                if not (k+val) in new_dist:
+                    new_dist[k+val] = 0
+
+                new_dist[k+val] += prob_val
+        dist = new_dist
+        # prune low probability dists
+        if prune_val is not None:
+            remove_keys = [k for k in dist.keys() if dist[k]<prune_val]
+            for k in remove_keys:
+                del dist[k]
+    
+    # values and probabilities
+    return np.array(list(dist.keys()))/avg_count, np.array(list(dist.values()))
+
+def solve_CTMC(Q):
+    # solve CTMC for pi
+    state_count = Q.shape[0]
+    Q[:, 0] = 1
+    y = np.zeros((1, Q.shape[0]))
+    y[0, 0] = 1
+    solution = np.linalg.solve(np.array(Q.T), np.array(y.T))
+    solution = solution.reshape(solution.shape[0],)
+    solution[solution < 0] = 0
+    return solution
+
