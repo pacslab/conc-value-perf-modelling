6b12c860eb63124696296028928fb144a6e3802e
Nima Mahmoudi <nima_mahmoudi@live.com>
2021-01-03 21:20:10 -0500

    seems like we are going the wrong direction, rewriting transition probability calculation

--- model/concperf/general_model.py
+++ model/concperf/general_model.py
@@ -1,4 +1,19 @@
+import itertools
+
 import numpy as np
+import scipy as sp
+from .single_model import StateCoder as SingleStateCoder
+
+class StateCoder(SingleStateCoder):
+    def __init__(self, config, ):
+        self.state_list = StateCoder.generate_state_list(config['max_container_count'])
+
+    # Generate State List
+    @staticmethod
+    def generate_state_list(max_conc):
+        cont_counts = list(range(max_conc + 1))
+        state_list = list(itertools.product(cont_counts, cont_counts, ))
+        return state_list
 
 def get_min_max_new_order(config):
     ready_count = config['instance_count']
@@ -29,4 +44,37 @@ def get_new_order_dist(req_count_averaged_vals, req_count_averaged_probs, config
             dist_ordered_inst[inst_count] += prob
 
     return np.array(list(dist_ordered_inst.keys())), np.array(list(dist_ordered_inst.values()))
-    
+
+
+def get_prov_trans_probs(ready_count, next_ready_counts, provision_rate_base, deprovision_rate_base, max_t=2):
+    state_count = len(next_ready_counts)
+    next_ready_counts = np.array(next_ready_counts)
+
+    ready_idx = np.where(next_ready_counts==ready_count)
+    if len(ready_idx) == 0:
+        raise ValueError('ready_count not found in next_ready_counts')
+    ready_idx = ready_idx[0][0]
+
+    Q = np.zeros((state_count, state_count))
+    # our initial state
+    init_state = np.zeros(state_count)
+    init_state[ready_idx] = 1
+
+    # the first one and last one cannot be source states (they are absorbing)
+    for i, next_ready_count in enumerate(next_ready_counts):
+        if i == 0 or i == next_ready_counts[-1]:
+            continue
+
+        rate = np.abs(next_ready_count - ready_count)
+        if ready_count <= next_ready_count:
+            rate *= provision_rate_base
+            Q[i, i+1] = rate
+        else:
+            rate *= deprovision_rate_base
+            Q[i, i-1] = rate
+
+        Q[i,i] = -1 * rate
+
+    solution = init_state @ sp.linalg.expm(Q * max_t)
+    return solution, Q
+
--- model/concperf/single_model.py
+++ model/concperf/single_model.py
@@ -3,36 +3,36 @@ import numpy as np
 
 # generate states and encode/decode between idx and state
 class StateCoder:
-  def __init__(self, config, ):
-    self.state_list = StateCoder.generate_state_list(config['max_conc'])
+    def __init__(self, config, ):
+        self.state_list = StateCoder.generate_state_list(config['max_conc'])
 
-  # Generate State List
-  @staticmethod
-  def generate_state_list(max_conc):
-    cont_counts = list(range(max_conc + 1))
-    state_list = list(itertools.product(cont_counts, ))
-    return state_list
+    # Generate State List
+    @staticmethod
+    def generate_state_list(max_conc):
+        cont_counts = list(range(max_conc + 1))
+        state_list = list(itertools.product(cont_counts, ))
+        return state_list
 
-  # Encode State to idx
-  def to_idx(self, state):
-    idx = self.state_list.index(state)
-    if idx < 0:
-      raise ValueError('state not found')
+    # Encode State to idx
+    def to_idx(self, state):
+        idx = self.state_list.index(state)
+        if idx < 0:
+            raise ValueError('state not found')
 
-    return idx
+        return idx
 
-  # Decode State from idx to state
-  def to_state(self, idx):
-    return self.state_list[idx]
+    # Decode State from idx to state
+    def to_state(self, idx):
+        return self.state_list[idx]
 
-  def get_state_list(self):
-    return self.state_list
+    def get_state_list(self):
+        return self.state_list
 
-  def get_state_count(self):
-    return len(self.state_list)
+    def get_state_count(self):
+        return len(self.state_list)
 
-  def get_state_name_list(self):
-    return [f"{s[0]},{s[1]},{s[2]}" for s in self.state_list]
+    def get_state_name_list(self):
+        return [f"{s[0]},{s[1]},{s[2]}" for s in self.state_list]
 
 
 # calculate Q for states
