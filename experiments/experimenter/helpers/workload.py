33d469ab5482c6aaf16fcd051613ab8773faeb83
Nima Mahmoudi <nima_mahmoudi@live.com>
2021-01-22 17:46:53 +0000

    added workload from requester

---
+++ experiments/experimenter/helpers/workload.py
@@ -0,0 +1,102 @@
+#! /usr/local/bin/python
+
+import os
+import sys
+import time
+import threading
+import random
+
+import requests
+
+from pacswg.timer import TimerClass
+
+http_path = os.getenv('HTTP_PATH')
+logger_path = os.getenv('LOGGER_PATH')
+exp_name = os.getenv('EXP_NAME')
+
+if http_path is None:
+    print("HTTP_PATH environment variable is necessary!")
+    sys.exit(1)
+
+print("Loaded HTTP path:", http_path)
+
+conc_count = 0
+conc_lock = threading.Lock()
+
+def get_conc():
+    global conc_count
+    return conc_count
+
+def inc_conc():
+    global conc_count
+    with conc_lock:
+        conc_count += 1
+
+def dec_conc():
+    global conc_count
+    with conc_lock:
+        conc_count -= 1
+
+def reset_conc():
+    global conc_count
+    with conc_lock:
+        conc_count = 0
+
+def report_conc_loop():
+    global conc_count
+
+    if logger_path is None or exp_name is None:
+        print('Report disabled, LOGGER_PATH or EXP_NAME not set.')
+        return
+
+    timer = TimerClass()
+    while True:
+        timer.tic()
+        res = requests.post(f'{logger_path}/logger/conc_logs/{exp_name}/client', data={'conc_value': conc_count})
+        if res.text != 'OK':
+            print('--- client push concurrency failed:', res.status, res.text)
+        while timer.toc() < 2:
+            time.sleep(0.01)
+
+def worker_func():
+    cmds = {}
+    cmds['sleep'] = 0
+    cmds['sleep_till'] = 0
+    cmds['stat'] = {"argv": 1}
+
+    # cmds['cpu'] = {"n": 20000}
+
+    cmds['sleep'] = 1000 + (random.random() * 200)
+
+    # cmds['io'] = {"rd": 3, "size": "200K", "cnt": 5}
+    # cmds['cpu'] = {"n": 10000}
+
+    payload = {}
+    payload['cmds'] = cmds
+
+    inc_conc()
+    try:
+        start_conc = conc_count
+        client_start_time = time.time()
+        res = requests.post(http_path, json=payload)
+        client_end_time = time.time()
+        end_conc = conc_count
+        # r_parsed = res.json()
+    except:
+        client_start_time = -1
+        client_end_time = -1
+        end_conc = -1
+    finally:
+        dec_conc()
+
+    return {
+        'client_start_time': client_start_time,
+        'client_end_time': client_end_time,
+        'client_elapsed_time': client_end_time - client_start_time,
+        'start_conc': start_conc,
+        'end_conc': end_conc,
+    }
+
+if __name__ == '__main__':
+    print(worker_func())
+    # report_conc_loop()
