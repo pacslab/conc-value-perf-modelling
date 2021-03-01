ac207693c5fc1c87e447e4dd9701f377ccc16dcb
Nima Mahmoudi <nima_mahmoudi@live.com>
2021-03-01 20:16:58 +0000

    added the basis for multiple plots

--- experiments/parse_util.py
+++ experiments/parse_util.py
@@ -3,6 +3,7 @@ from IPython import get_ipython
 # imports for plots
 import pacsltk.pacs_util as pacs_util
 import pandas as pd
+import numpy as np
 
 pacs_util.prepare_matplotlib_cycler()
 
@@ -65,9 +66,39 @@ def parse_logger_file(csv_file_path, parse_skip_mins=0):
     parse_epoch_cols(df, ['time', ])
     # skip 5 minutes
     df = df[df['time_dt'] > df['time_dt'].min() + timedelta(minutes=parse_skip_mins)]
-    # df = df[df['time_dt'] < df['time_dt'].min() + timedelta(minutes=10)]
+    # df = df[df['time_dt'] < df['time_dt'].min() + timedelta(minutes=20)]
     df['lambda_over_n'] = df['rps'] / df['ready_count']
     df['current_cc'] = df['total_conc'] / df['ready_count']
     # set index to the time
     df = df.set_index('time_dt')
     return df
+
+# parse in batches with custom functions and return mean, var, and se
+def parse_batch_custom_funcs(df, batch_seconds, parse_cols_funcs, start_date=None, stop_date=None):
+    if start_date is None:
+        start_date = df.index.min()
+
+    if stop_date is None:
+        stop_date = df.index.max()
+
+    range_start_dates = pd.date_range(start_date, stop_date,freq=f'{batch_seconds}S')
+    # create end dates and drop first and last ones
+    range_end_dates = range_start_dates.shift()[1:-1]
+    range_start_dates = range_start_dates[1:-1]
+
+    results = {}
+    for parse_col_name in parse_cols_funcs:
+        val_means = []
+        for idx in range(len(range_start_dates)):
+            sub_df = df.loc[(df.index < range_end_dates[idx]) & (df.index > range_start_dates[idx]), :]
+            val_mean = parse_cols_funcs[parse_col_name](sub_df)
+            val_means.append(val_mean)
+
+        val_means_mean = np.mean(val_means)
+        val_means_var = np.var(val_means)
+
+        results[f"{parse_col_name}_mean"] = val_means_mean
+        results[f"{parse_col_name}_var"] = val_means_var
+        results[f"{parse_col_name}_se"] = np.sqrt(val_means_var / len(val_means))
+
+    return results
