78c81538c3aafcca857d6ec6bc4fa7630db323b9
Nima Mahmoudi <nima_mahmoudi@live.com>
2021-05-27 18:07:24 +0000

    minor changes

--- model/analyze/extract_results.sh
+++ model/analyze/extract_results.sh
@@ -2,8 +2,8 @@
 
 rm model_results_figs.zip
 
-# zip -r model_results_figs.zip figs/* results/*
-zip -r model_results_figs.zip figs/*
+zip -r model_results_figs.zip figs/* results/*
+# zip -r model_results_figs.zip figs/*
 
 # automatic upload makes it easier to transfer files
 curl --upload-file ./model_results_figs.zip https://free.keep.sh
